9ddf463017846d870fd39a1dabaecdaa22335d0c
Julia Makeeva <juliamaakeeva@gmail.com>
2023-03-29 00:09:09 +0300

    commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,11 +2,13 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Dog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
-
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
